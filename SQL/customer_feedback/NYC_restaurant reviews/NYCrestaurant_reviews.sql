d5e6824891904a916fc2f82fee52ea6c1c522a9d
Jonathan Jones <89792468+Get2dacode@users.noreply.github.com>
2021-10-01 11:24:49 -0400

    Add files via upload

---
+++ SQL/customer_feedback/NYC_restaurant reviews/NYCrestaurant_reviews.sql
@@ -0,0 +1,58 @@
+-- select *
+-- From ny_restaurants
+
+-- select distinct neighborhood
+-- From ny_restaurants
+
+-- select distinct neighborhood
+-- From ny_restaurants
+
+-- select *
+-- From ny_restaurants
+-- WHERE cuisine = 'Chinese'
+
+-- select *
+-- From ny_restaurants
+-- WHERE review >= 4
+
+-- select *
+-- From ny_restaurants
+-- WHERE cuisine = 'Italian' and price = '$$$'
+
+-- select *
+-- From ny_restaurants
+-- -- WHERE name LIKE '_eatball';
+
+-- select *
+-- From ny_restaurants
+-- WHERE neighborhood = 'Midtown' or neighborhood = 'Downtown' or neighborhood = 'Chinatown'
+
+-- SELECT *
+-- FROM ny_restaurants
+-- WHERE health = null ;
+
+-- SELECT *
+-- FROM ny_restaurants
+-- ORDER BY review DESC
+-- LIMIT 10;
+
+-- SELECT 
+-- CASE
+-- WHEN review > 4.5 then 'Extraordinary'
+-- WHEN review > 4 then  'Excellent'
+-- WHEN review > 3 then  'Good'
+-- WHEN review > 2 then  'Fair'
+-- WHEN review <= 2 then  'Poor'
+-- 
+-- END as restaurant_reviews
+-- FROM ny_restaurants;
+
+
+
+
+
+
+
+
+
+
